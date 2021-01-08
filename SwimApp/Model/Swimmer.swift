6dbbcf9c65fd3521edc575a0b5d241883e48d14c
Chadwick Leung <chadwickleung.wcl@gmail.com>
2021-01-08 15:39:34 -0800

    added structs

---
+++ SwimApp/Model/Swimmer.swift
@@ -0,0 +1,16 @@
+//
+//  Swimmer.swift
+//  SwimApp
+//
+//  Created by Chadwick Leung on 1/8/21.
+//
+
+import Foundation
+
+struct Swimmer {
+    
+    var name: String
+    var dateOfBirth: Date
+    var gender: String
+    
+}
---
+++ SwimApp/Model/TrainingSet.swift
@@ -0,0 +1,18 @@
+//
+//  TrainingSet.swift
+//  SwimApp
+//
+//  Created by Chadwick Leung on 1/8/21.
+//
+
+import Foundation
+
+struct TrainingSet {
+    
+    var distance: Int
+    var rep: Int
+    var set: Int
+    
+    var stroke: String
+    var description: String
+}
