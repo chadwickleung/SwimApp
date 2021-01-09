44de2522ddc43da625036c178b647b6289ef54e1
Chadwick Leung <chadwickleung.wcl@gmail.com>
2021-01-08 16:53:02 -0800

    added some classes

--- SwimApp/Model/Swimmer.swift
+++ SwimApp/Model/Swimmer.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
-struct Swimmer {
+class Swimmer {
+    var _name: String
+    var _gender: String          // M or F
+    var _dateOfBirth: String
+    var _year: Int
     
-    var name: String
-    var dateOfBirth: Date
-    var gender: String
+    // [trainingSetID: [Record]]
+    var _trainingRecord: [Int: [Float]]?
     
+    init(name: String, gender: String, dateOfBirth: String, year: Int) {
+        _name = name
+        _gender = gender
+        _dateOfBirth = dateOfBirth
+        _year = year
+        _trainingRecord = nil
+    }
 }
--- SwimApp/Model/TrainingSet.swift
+++ SwimApp/Model/TrainingSet.swift
@@ -7,12 +7,32 @@
 
 import Foundation
 
+// This structure holds the information of the training set
 struct TrainingSet {
-    
+    // Format: SET * (REP * DISTANCE)
     var distance: Int
     var rep: Int
     var set: Int
     
     var stroke: String
     var description: String
+    
+    var participants: [Swimmer]
 }
+
+
+
+// This structure holds all the training sets in the swim set
+class SwimSet {
+    var trainingSets: [TrainingSet]
+    
+    init() {
+        self.trainingSets = []
+    }
+    
+    func addTrainingSet(set: TrainingSet) {
+        trainingSets.append(set)
+    }
+    
+}
+\
